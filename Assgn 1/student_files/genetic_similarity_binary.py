50eb56488bcda316db672e624fe383e911e3a17a
nickhuang1997 <njgh002@bucknell.edu>
2017-08-25 16:23:35 +1200

    final commit

--- Assgn 1/student_files/genetic_similarity_binary.py
+++ Assgn 1/student_files/genetic_similarity_binary.py
@@ -21,69 +21,53 @@ def genetic_similarity_binary(first_genome, second_genome):
         how many comparisons it took to find all the similar genes.
         Hint: it might pay to define a helper function.
     """
-    GA_list = Genome_to_GeneList(first_genome)
-    GB_list = Genome_to_GeneList(second_genome)
+    ga_list = genome_to_genelist(first_genome)
+    gb_list = genome_to_genelist(second_genome)
     
-    return binary_search(GA_list, GB_list)
+    return binary_search(ga_list, gb_list)
 
-def Genome_slicer(filename):
-    """Calls the read_test_data function to output slices genomes
-    """
-    import utilities
-    genome_a, genome_b, commone_genes = utilities.read_test_data(filename)
-    return genome_a , genome_b, commone_genes
-
-
-def Genome_to_GeneList(genome_a):
+def genome_to_genelist(genome_a):
     """Takes each sequence from each genomes and puts it into GeneList"""
     
-    GL = GeneList()
+    initialgenelist = GeneList()
     for i in range(len(genome_a)):
-        GL.append(genome_a[i])                 #puts item genome_a[i] into GeneList
-    return GL
-
+        initialgenelist.append(genome_a[i])                 #puts item genome_a[i] into GeneList
+    return initialgenelist
 
 def binary_search(genome_a, genome_b):
     """uses a binary search to find common genomes genome_a and genome_b
     genome_b must be in lexicographic order
     """
-    GL1 = Genome_to_GeneList(genome_a)
-    GL2 = Genome_to_GeneList(genome_b) 
     count = 0
     common_genes = GeneList()
     
-    for i in range(len(GL1)):
-        find_this_genome = GL1[i]
-#        print(find_this_genome)
+    for i in range(len(genome_a)):
+        find_this_genome = genome_a[i]
         lower = 0
-        upper = len(GL2)
+        upper = len(genome_b) - 1
 
-        while lower <= upper:               
-            midpoint = (upper + lower)//2               
+        while lower < upper:               
+            midpoint = (upper + lower)//2                           
+            
             count += 1
-#            print("mid is "+str(midpoint))
-            
-            if find_this_genome == GL2[midpoint]:
-                common_genes.append(find_this_genome)
-#                print('Found it! '+ str(GL2[midpoint]))
-                break
-            
-            elif find_this_genome < GL2[midpoint]:
-                if upper == midpoint:
-                    break
+            if find_this_genome <= genome_b[midpoint]:
                 upper = midpoint
-                
-            elif find_this_genome > GL2[midpoint]: 
-                if lower == midpoint:
-                    break
-                lower = midpoint
-                
-    print(count)
-    print(common_genes)
+            else:
+                lower = midpoint + 1
+
+        count += 1
+        #print(midpoint)
+        if find_this_genome == genome_b[upper]:
+            #print(find_this_genome)
+            common_genes.append(find_this_genome)
+
+                    
+    #print(count)
+    #print(common_genes)
     return common_genes, count
     
 
-a,b,c = Genome_slicer('TestData/test_data-1000-0.txt')
-genetic_similarity_binary(a,b)
+#a,b,c = Genome_slicer('TestData/test_data-1000-250.txt')
+#binary_search(a,b)
+#genetic_similarity_binary(a,b)
 
-    
